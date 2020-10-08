aee29aeb076699dbdee56724d20106fd94ee539b
Michael Kim <m.kim4247@gmail.com>
2020-10-08 20:28:50 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,3 @@
-def my_select(collection)
- i = 0 
- arr = []
- while i < collection.length 
- if yield(collection[i]) == true 
-   arr.push(collection[i])
- end 
- i += 1 
- end 
- arr 
+def my_select
+  
 end
